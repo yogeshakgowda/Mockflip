2de1dd6c5c321f842d019b14b3ce3281c3b1ac82
yogeshakgowda <yogesha.052k@gmail.com>
2024-06-07 10:51:32 +0530

    pc

--- MockFrame/src/test/java/flipkart/MockFlipkartTest.java
+++ MockFrame/src/test/java/flipkart/MockFlipkartTest.java
@@ -17,25 +17,25 @@ import commclass.MockBaseclass;
 public class MockFlipkartTest extends MockBaseclass
 {
 	//@Test
-	public void script() throws MalformedURLException, InterruptedException
-	{
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		driver.get("https://www.flipkart.com/");
-		Thread.sleep(3000);
-		flip.searchiphone("iphone 15");
-		List<WebElement> phone = driver.findElements(By.xpath("//div[contains(text(),'Apple iPhone 15')]"));
-		TreeSet<String> sorted=new TreeSet<String>(Collections.reverseOrder());
-		for(WebElement ele:phone)
-		{
-			String txt = ele.getText();
-			sorted.add(txt);
-		}
-		for(String iphon:sorted)
-		{
-		System.out.print(iphon+" ");
-		WebElement price = driver.findElement(By.xpath("//div[contains(text(),'"+iphon+"')]/../following-sibling::div//div[contains(text(),'₹')]"));
-		System.out.print(price.getText());
-		System.out.println();
-		}
-	}
+//	public void script() throws MalformedURLException, InterruptedException
+//	{
+//		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//		driver.get("https://www.flipkart.com/");
+//		Thread.sleep(3000);
+//		flip.searchiphone("iphone 15");
+//		List<WebElement> phone = driver.findElements(By.xpath("//div[contains(text(),'Apple iPhone 15')]"));
+//		TreeSet<String> sorted=new TreeSet<String>(Collections.reverseOrder());
+//		for(WebElement ele:phone)
+//		{
+//			String txt = ele.getText();
+//			sorted.add(txt);
+//		}
+//		for(String iphon:sorted)
+//		{
+//		System.out.print(iphon+" ");
+//		WebElement price = driver.findElement(By.xpath("//div[contains(text(),'"+iphon+"')]/../following-sibling::div//div[contains(text(),'₹')]"));
+//		System.out.print(price.getText());
+//		System.out.println();
+//	}
+//	}
 }
